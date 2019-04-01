63e56f4e877c8057936ac2b8f2afdfb3e3f137ec
Jasper Speicher <jasper.speicher@gmail.com>
2019-03-31 23:11:40 -0700

    Replace transitions code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,25 @@
 import { Component, OnInit } from '@angular/core';
-import {AppService} from "./app.service";
-import {MenuService} from "./menu/menu.service";
+import { AppService } from './app.service';
+import { MenuService } from './menu/menu.service';
+import { fadeAnimation } from './shared/fade.animation';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [AppService, MenuService]
+  providers: [AppService, MenuService],
+  animations: [fadeAnimation]
 })
 export class AppComponent implements OnInit {
 
-  title:string = '';
+  title = '';
 
-  constructor(private appService:AppService) {
+  constructor(private appService: AppService) {
 
+  }
+
+  public getRouterOutletState(outlet) {
+    return outlet.isActivated ? outlet.activatedRoute : '';
   }
 
   ngOnInit() {
