df2c273ed29f85c2a5ecc54241c89ce13e778bac
Jasper Speicher <jasper.speicher@gmail.com>
2019-04-01 22:47:24 -0700

    Get rid of transitions import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,12 +7,12 @@ import { AppComponent } from './app.component';
 import { PostListComponent } from './posts/post-list/post-list.component';
 import { Wpng2RoutingModule } from './app-routing.module';
 import { PostSingleComponent } from './posts/post-single/post-single.component';
-import { PageSingleComponent } from "./pages/page-single/page-single.component";
-import { PageHomeComponent } from "./pages/page-home/page-home.component";
+import { PageSingleComponent } from './pages/page-single/page-single.component';
+import { PageHomeComponent } from './pages/page-home/page-home.component';
 import { MenuComponent } from './menu/menu.component';
 
-import { Ng2PageTransition } from "ng2-page-transition";
 import { PackeryComponent } from './packery/packery.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 
 @NgModule({
@@ -23,14 +23,14 @@ import { PackeryComponent } from './packery/packery.component';
     PageSingleComponent,
     PageHomeComponent,
     MenuComponent,
-    Ng2PageTransition,
     PackeryComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
-    Wpng2RoutingModule
+    Wpng2RoutingModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
