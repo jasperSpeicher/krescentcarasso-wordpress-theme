8f4287dfbe758438b3dae2f3cd6e63087270c532
Jasper Speicher <jasper.speicher@gmail.com>
2020-01-05 19:41:09 -0800

    Add base url var

--- src/app/app.service.ts
+++ src/app/app.service.ts
@@ -5,12 +5,12 @@ import { Observable } from 'rxjs/Observable';
 
 import 'rxjs/add/operator/map';
 
+const baseUrl = 'http://krescentcarasso.jasperspeicher.codes'; //window ? window.location.origin : 'http://krescentcarasso.jasperspeicher.codes';
 
 @Injectable()
 export class AppService {
 
-  //public static _wpBase = "http://localhost:8888/krescentcarasso/wp-json/";
-  public static _wpBase = "http://krescentcarasso.jasperspeicher.codes/wp-json/";
+  public static _wpBase = `${baseUrl}/wp-json/`;
 
   constructor(private http:Http) {
   }
