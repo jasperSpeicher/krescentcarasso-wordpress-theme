c2198f80cd8371f41cdec42ded2c6e17a78c0dcc
Jasper Speicher <jasper.speicher@gmail.com>
2020-01-09 16:17:53 -0800

    Delay routes so menu can close

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,28 +1,44 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { PostListComponent } from './posts/post-list/post-list.component';
-import { PostSingleComponent } from './posts/post-single/post-single.component';
-import { PageSingleComponent } from "./pages/page-single/page-single.component";
-import { PageHomeComponent } from "./pages/page-home/page-home.component";
+import { PageSingleComponent } from './pages/page-single/page-single.component';
+import { PageHomeComponent } from './pages/page-home/page-home.component';
+import { Observable } from 'rxjs';
+import { Injectable } from '@angular/core';
+import { Resolve } from '@angular/router';
 
-const routes:Routes = [
+@Injectable()
+export class DelayResolve implements Resolve<Observable<any>> {
+
+  constructor() {
+  }
+
+  resolve(): any {
+    return Observable.of('delayed navigation').delay(400);
+  }
+}
+
+const routes: Routes = [
 
   {
     path: ':parent/:slug',
     component: PageSingleComponent,
+    resolve: [DelayResolve],
   },
   {
     path: 'explore',
     redirectTo: 'explore/all',
+    resolve: [DelayResolve],
   },
   {
     path: '',
     component: PageHomeComponent,
-    pathMatch: 'full'
+    pathMatch: 'full',
+    resolve: [DelayResolve],
   },
   {
-    path: "**",
-    redirectTo:''
+    path: '**',
+    redirectTo: '',
+    resolve: [DelayResolve],
   }
 
 ];
@@ -30,7 +46,8 @@ const routes:Routes = [
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule],
-  providers: []
+  providers: [DelayResolve]
 })
 export class Wpng2RoutingModule {
 }
+
