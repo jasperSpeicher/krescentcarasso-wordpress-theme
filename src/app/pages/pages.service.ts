5acb26d5722df899daefefb2bdd96883aeb4e472
Jasper Speicher <jasper.speicher@gmail.com>
2020-01-26 17:40:12 -0800

    Load pages once at init

--- src/app/pages/pages.service.ts
+++ src/app/pages/pages.service.ts
@@ -2,9 +2,8 @@ import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
 import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/last';
 import 'rxjs/add/observable/forkJoin';
-import 'rxjs/add/observable/range';
+import 'rxjs/add/observable/of';
 
 import { Page } from './page';
 import { AppService } from '../app.service';
@@ -13,26 +12,25 @@ import { AppService } from '../app.service';
 export class PagesService {
 
   private _wpBase = AppService._wpBase;
+  private pages = null;
+  private pagesRequest = null;
 
   constructor(private http: Http) {
+    this.pagesRequest = this.getPages();
+    this.pagesRequest.subscribe(res => this.pages = res);
   }
 
   getPages(): Observable<Page[]> {
-
-    return this.http
-      .get(this._wpBase + 'wp/v2/pages')
-      .map((res: Response) => res.json());
-
+    return (this.pages && Observable.of(this.pages)) ||
+      (this.pagesRequest ||
+        this.http
+          .get(this._wpBase + 'wp/v2/pages')
+          .map((res: Response) => res.json()));
   }
 
   getPage(slug): Observable<Page> {
-
-    return this.http
-      .get(this._wpBase + 'wp/v2/pages?slug=' + slug)
-      .map((res: Response) => {
-        return res.json();
-      });
-
+    return ((this.pages && Observable.of(this.pages)) || this.pagesRequest)
+      .map(pages => pages.filter(p => p.slug === slug));
   }
 
   getMediaObjects(): Observable<any> {
